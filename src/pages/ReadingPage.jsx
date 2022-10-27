90020341a6d846301c16aa76ba8766f098310a39
Oleh Subotin <subotinoleg0@gmail.com>
2022-10-27 16:31:45 +0300

    reading exercise 1-2

--- src/pages/ReadingPage.jsx
+++ src/pages/ReadingPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Container from 'components/VocabularyPageContent/Container';
+import Section from 'components/Section';
+import Task from 'components/VocabularyPageContent/Task';
+import Text from 'components/ReadingPageContent/Text';
+import Select from "components/ReadingPageContent/Select";
+import DBText from '../db/readingText';
 
 const ReadingPage = () => {
   return (
-    <div>ReadingPage</div>
+    <Container>
+      <Section>
+        <Task
+          accentText="Exercise 1"
+          text="Read an invitation to an office event to practise and improve your reading skills. Do the preparation task first. Then read the text and do the exercises."
+        />
+        <Text text={DBText[0].text} title={DBText[0].title} />
+      </Section>
+      <Section>
+        <Task
+          accentText="Exercise 2"
+          text="Choose correct option"
+        />
+        <Select tasks={DBText[0].task1} />
+      </Section>
+    </Container>
   )
 }
 
